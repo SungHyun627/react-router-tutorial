5dacdfe1f83ea082ec25788459f3f60a9cc2e10f
Sunghyun627 <kksscc4@gmail.com>
2022-03-29 17:51:23 +0900

    feat: create route config

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Expenses from './routes/expenses';
+import Invoices from './routes/invoices';
+
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route path="expenses" element={<Expenses />}></Route>
+      <Route path="invoices" element={<Invoices />}></Route>
+    </Routes>
   </BrowserRouter>,
   document.getElementById('root')
 );
