b55a757c6f5bf14d7832c4f3a6a852bbaca7fbfd
Sunghyun627 <kksscc4@gmail.com>
2022-03-30 12:04:39 +0900

    feat: implement nested routes/UI components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { Link, Outlet } from 'react-router-dom';
+
 export default function App() {
   return (
     <div>
       <h1>Bookeeper!</h1>
+      <nav>
+        <Link to="/invoices">Invoices</Link>
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Expenses from './routes/expenses';
 import Invoices from './routes/invoices';
@@ -8,9 +9,10 @@ import Invoices from './routes/invoices';
 ReactDOM.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}></Route>
-      <Route path="expenses" element={<Expenses />}></Route>
-      <Route path="invoices" element={<Invoices />}></Route>
+      <Route path="/" element={<App />}>
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+      </Route>
     </Routes>
   </BrowserRouter>,
   document.getElementById('root')
