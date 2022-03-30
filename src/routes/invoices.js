62ff0341e67adc56738826362556202e94ae4561
Sunghyun627 <kksscc4@gmail.com>
2022-03-30 19:51:40 +0900

    feat: add filter feature using search params

--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -1,22 +1,42 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useSearchParams } from 'react-router-dom';
 import { getInvoices } from '../data';
 
 export default function Invoices() {
   let invoices = getInvoices();
+  let [searchParams, setSearchParams] = useSearchParams();
+
   return (
     <div className="invoice-list">
       <nav>
-        {invoices.map((invoice) => (
-          <NavLink
-            style={({ isActive }) => {
-              return { color: isActive ? 'red' : '' };
-            }}
-            to={`/invoices/${invoice.number}`}
-            key={invoice.number}
-          >
-            {invoice.name}
-          </NavLink>
-        ))}
+        <input
+          value={searchParams.get('filter') || ''}
+          onChange={(e) => {
+            let filter = e.target.value;
+            if (filter) {
+              setSearchParams({ filter });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
+        {invoices
+          .filter((invoice) => {
+            let filter = searchParams.get('filter');
+            if (!filter) return true;
+            let name = invoice.name.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          })
+          .map((invoice) => (
+            <NavLink
+              style={({ isActive }) => {
+                return { color: isActive ? 'red' : '' };
+              }}
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </NavLink>
+          ))}
       </nav>
       <Outlet />
     </div>
