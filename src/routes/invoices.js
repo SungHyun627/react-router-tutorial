121ecf9e9776d14d9e09a17f66931035486a73e5
Sunghyun627 <kksscc4@gmail.com>
2022-03-30 12:23:29 +0900

    feat: create invoice list

--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -1,7 +1,17 @@
+import { Link } from 'react-router-dom';
+import { getInvoices } from '../data';
+
 export default function invoices() {
+  let invoices = getInvoices();
   return (
-    <main>
-      <h2>Invoices</h2>
-    </main>
+    <div className="list">
+      <nav>
+        {invoices.map((invoice) => (
+          <Link to={`/invoices/${invoice.number}`} key={invoice.number}>
+            {invoice.name}
+          </Link>
+        ))}
+      </nav>
+    </div>
   );
 }
