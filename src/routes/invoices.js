6e7af452d8459975dad826e6016ef7fe3ad8b3fd
Sunghyun627 <kksscc4@gmail.com>
2022-03-30 12:34:14 +0900

    fix: fix invoice'list element's class name

--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -4,7 +4,7 @@ import { getInvoices } from '../data';
 export default function invoices() {
   let invoices = getInvoices();
   return (
-    <div className="list">
+    <div className="invoice-list">
       <nav>
         {invoices.map((invoice) => (
           <Link to={`/invoices/${invoice.number}`} key={invoice.number}>
