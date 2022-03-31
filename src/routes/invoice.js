128dc794dc59088d157bc1b46e2164b234077360
Sunghyun627 <kksscc4@gmail.com>
2022-03-31 09:56:46 +0900

    feat: add button to delete button and navigte to index route

--- src/routes/invoice.js
+++ src/routes/invoice.js
@@ -1,9 +1,12 @@
-import { useParams } from 'react-router-dom';
-import { getInvoice } from '../data';
+import { useParams, useLocation, useNavigate } from 'react-router-dom';
+import { getInvoice, deleteInvoice } from '../data';
 
 export default function Invoice() {
   let params = useParams();
+  let navigate = useNavigate();
+  let location = useLocation();
   let invoice = getInvoice(parseInt(params.invoiceId, 10));
+  console.log(location);
   return (
     <main>
       <h2>Total Due: {invoice.amount}</h2>
@@ -11,6 +14,16 @@ export default function Invoice() {
         {invoice.name}: {invoice.number}
       </p>
       <p>Due Date: {invoice.due}</p>
+      <p>
+        <button
+          onClick={() => {
+            deleteInvoice(invoice.number);
+            navigate('/invoices' + location.search);
+          }}
+        >
+          Delte
+        </button>
+      </p>
     </main>
   );
 }
